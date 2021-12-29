ad3ef2b8cac7670ea6898acefa813f20f3dc4f90
José Pascual <josepascual.manzano@gmail.com>
2021-12-30 00:31:38 +0100

    sintaxis-funciones exercise 6

---
+++ ejercicios/sintaxis-funciones/ejercicio-06.js
@@ -0,0 +1,15 @@
+/*
+Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, 
+es decir, si se lee de la misma forma desde la izquierda y desde la derecha. 
+Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural".
+*/
+
+function isPalindrome(text) {
+  const textStraight = text.trim().split(" ").join("").toLowerCase();
+  const textReversed = text.trim().split(" ").join("").split("").reverse().join("").toLowerCase();
+  return textStraight === textReversed;
+}
+
+console.log(isPalindrome("La ruta nos aporto otro paso natural"));
+console.log(isPalindrome("Amore Roma"));
+console.log(isPalindrome("caramaroc"));
